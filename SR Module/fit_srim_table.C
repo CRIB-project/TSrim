272d88ba15719911a9450dc4847714b469b6d175
Seiya Hayakawa <hayakawa@cns.s.u-tokyo.ac.jp>
2024-06-27 14:11:41 +0900

    perl script, root macro for range fit in SR Module/ dir

--- SR Module/fit_srim_table.C
+++ SR Module/fit_srim_table.C
@@ -1,1 +1,197 @@
-../../SR Module/fit_srim_table.C
+/* #include <TString.h> */
+/* #include <TStyle.h> */
+/* #include <TApplication.h> */
+/* #include <vector> */
+/* #include <iostream> */
+/* #include <fstream> */
+/* #include <string.h> */
+/* #include <TGraph.h> */
+/* #include <TAxis.h> */
+/* #include <TF1.h> */
+/* #include <TCanvas.h> */
+/* #include <TPad.h> */
+/* #include <TSystem.h> */
+//#include <TROOT.h>
+
+/* using namespace std; */
+
+
+// 平均を計算する関数
+Double_t calculateMean(const std::vector<Double_t>& data) {
+  Double_t sum = 0.0;
+  for(Double_t num : data) {
+    sum += num;
+  }
+  return sum / data.size();
+}
+
+// 標準偏差を計算する関数
+Double_t calculateStdDev(const std::vector<Double_t>& data) {
+  Double_t mean = calculateMean(data);
+  Double_t standardDeviation = 0.0;
+  
+  for(Double_t num : data) {
+    standardDeviation += pow(num - mean, 2);
+  }
+
+  return sqrt(standardDeviation / data.size());
+}
+
+/* int main( */
+void fit_srim_table(Int_t Z, Int_t A, const Char_t *mat, const Char_t *fin,
+		    const Int_t npol, const Char_t *fout, Option_t* option=""){
+/*   Int_t Z; */
+/*   Int_t A; */
+/*   const Char_t *mat; */
+/*   const Char_t *fin; */
+/*   const Int_t npol = 16; */
+/*   const Char_t *fout; */
+/*   Option_t* option=""; */
+  TString opt = option; // c = canvas, w = write to file 
+  opt.ToLower();
+  gStyle->SetMarkerStyle(2);
+  gStyle->SetMarkerSize(0.5);
+  //TApplication theApp("App", NULL, NULL);
+
+  Double_t dummy;
+  Char_t char_dummy[10];
+  Char_t e_unit[10];
+  Char_t r_unit[10];
+  Double_t E, dEdx, R;
+  vector<Double_t> vE, vR;
+  vector<Double_t> vdEdx; //, vdxdE;
+  vector<Double_t> vlogE, vlogR;
+  ifstream ftable(fin);
+  while( !ftable.eof() ){
+    ftable >> E >> e_unit >> dummy >> dEdx
+	   >> R >> r_unit >> dummy >> char_dummy >> dummy >> char_dummy;
+    if(ftable.eof())
+      break;
+    // Energy
+    if(!strcmp(e_unit,"eV"))
+      E /= 1000000.;
+    else if(!strcmp(e_unit,"keV"))
+      E /= 1000.;    
+    else if(!strcmp(e_unit,"GeV"))
+      E *= 1000.;    
+    vE.push_back(E); // in MeV
+    if(opt.Contains("c"))
+    // Stopping Power
+       vdEdx.push_back(dEdx);
+    /* vdxdE.push_back(1./dEdx); */
+    // RangeX
+    if(!strcmp(r_unit,"A"))
+      R /=10000000.;
+    else if(!strcmp(r_unit,"um"))
+      R /= 1000.;
+    else if(!strcmp(r_unit,"mm"))
+      R *= 1.;
+    else if(!strcmp(r_unit,"m"))
+      R *= 1000.;
+    else if(!strcmp(r_unit,"km"))
+      R *= 1000000.;
+    else{
+      cout << "Range out of range (<A or >km)" << endl;
+      return;
+    }
+      
+    vR.push_back(R); // in mm
+
+    vlogE.push_back(log10(E));
+    vlogR.push_back(log10(R));
+    //cout << E << " " << dEdx << endl;
+  }
+  ftable.close();
+
+  // Get the minimum and maximum energies
+  Double_t logEmin = vlogE.at(0);
+  Double_t logEmax = vlogE.back();
+  
+  TGraph *gr3=new TGraph(vlogE.size(),&(vlogE.at(0)),&(vlogR.at(0)));
+  /* gr3->Draw("ap"); */
+  TF1 *fn3=new TF1("fn3",Form("pol%d",npol),logEmin,logEmax);
+  gr3->Fit(fn3,"","",logEmin,logEmax);
+
+  // Get mean and standard deviation of (range fit - range)/range
+  vector<Double_t> vdRR;
+  vector<Double_t> vRfit;
+  Double_t logR;
+  for(Int_t i=0; i<vlogE.size(); i++){
+    logR = fn3->Eval(vlogE.at(i)); // Fit function at logE
+    vdRR.push_back( (pow(10,logR) - vR.at(i))/vR.at(i) );
+    if(opt.Contains("c"))
+      vRfit.push_back(pow(10,logR));
+  }
+
+  cout << endl;
+  cout << "Z = " << Z << ", A = " << A << " fitted"  << endl;
+  cout << "Chi2/NDf = " << fn3->GetChisquare()/fn3->GetNDF() << endl;
+  cout << "Mean +/- StdDev of (Range fit - Range)/Range: "
+       << calculateMean(vdRR) << "+/-" << calculateStdDev(vdRR) << endl;
+  cout << endl;
+  
+  // Write to File option
+  if(opt.Contains("w")){
+    ofstream fpar(fout, std::ios::app); //appending mode
+    fpar << Z << " " << A << " " << mat << " ";
+    for(Int_t j=0; j<npol+1; j++){
+      fpar << fn3->GetParameter(j) << " ";
+    }
+    //fpar << fn3->GetChisquare()/fn3->GetNDF();
+    fpar << calculateMean(vdRR) << " ";
+    fpar << calculateStdDev(vdRR);
+    fpar << endl;
+    fpar.close();
+    cout << Form("Parameters written in %s",fout) << endl;
+  }
+  
+  //// Draw to Canvas option
+  if(opt.Contains("c")){
+    TCanvas *c1=new TCanvas("c1","c1",0,0,800,800);
+    c1->Divide(2,2);
+  
+    c1->cd(1);
+    gPad->SetLogx(); 
+    TGraph *gr1=new TGraph(vE.size(),&(vE.at(0)),&(vdEdx.at(0)));
+    gr1->SetTitle("Energy (MeV) vs Stopping power (MeV/(mg/cm2))");
+    gr1->GetXaxis()->SetRangeUser(pow(10,logEmin),pow(10,logEmax));
+    gr1->Draw("ap");
+
+    c1->cd(2);
+    TGraph *gr2=new TGraph(vE.size(),&(vE.at(0)),&(vR.at(0)));
+    gr2->SetTitle("Energy (MeV) vs Range (mm)");
+    gr2->GetXaxis()->SetRangeUser(pow(10,logEmin),pow(10,logEmax));
+    gr2->Draw("ap");
+    gPad->SetLogx();
+    gPad->SetLogy();
+
+    c1->cd(3);
+    gr3->SetTitle("Log(Energy (MeV)) vs log(Range (mm))");
+    gr3->GetXaxis()->SetRangeUser(logEmin,logEmax);
+    gr3->Draw("ap");
+  
+    c1->cd(4);
+    TGraph *gr4=new TGraph(vlogE.size(),&(vlogE.at(0)),&(vdRR.at(0)));
+    gr4->SetTitle("Log(Energy) vs (Range fit - Range SRIM)/(Range SRIM)");
+    gr4->GetXaxis()->SetRangeUser(logEmin,logEmax);
+    gr4->Draw("ap");
+    
+    TGraph *gr3fit=new TGraph(vE.size(),&(vE.at(0)),&(vRfit.at(0)));
+    c1->cd(2);
+    gr3fit->SetLineColor(kRed);
+    gr3fit->SetLineStyle(1);
+    gr3fit->SetLineWidth(2);
+    gr3fit->Draw("l");
+
+    gr2->Draw("p");
+
+    c1->Update();
+    gSystem->ProcessEvents();
+  }
+  //theApp.Run();
+
+  if(!opt.Contains("c"))
+    gROOT->ProcessLine(".q");
+
+  //  return 0;
+}
--- SR Module/get_sr_fit_prm.pl
+++ SR Module/get_sr_fit_prm.pl
@@ -1,1 +1,149 @@
-../../SR Module/get_sr_fit_prm.pl
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+######################################################################
+# Execute this perl script in the directory where SRModule.exe exists.
+# Target material data file should be prepared in SR.IN format,
+# and place it in "material_name/" directory in advance.
+# A mass table file is needed in "Z A Element mass" format.
+######################################################################
+# Initial ion for loop
+my $abeg = 1; 
+my $elbeg = 'H';
+# my $abeg = 70; 
+# my $elbeg = 'Mn';
+# my $abeg = 56; 
+# my $elbeg = 'Ni';
+# my $abeg = 219;
+# my $elbeg = 'U';
+
+# Final ion for loop
+#my $aend = 295;
+#my $elend = 'Og';
+my $aend = 243;
+my $elend = 'U';
+# my $aend = 2;
+# my $elend = 'H';
+# my $aend = 56; 
+# my $elend = 'Ni';
+
+# Target material list
+#my @mat = ('h','butane','al','si');
+#my @mat = ('he','butane','al','si','havar','mylar','ch2');
+#my @mat = ('he');
+#my @mat = ('air');
+#my @mat = ('c','cd2','kapton','au');
+#my @mat = ('au','he_90_co2_10');
+my @mat = ('ar','mo','cu','3he');
+
+# Directory and mass table file name
+my $dir = 'range';
+my $fmass = '/usr/local/share/mass2020.dat'; # mass table
+my $label = ''; # if you need to label the output fit parameter file
+#my $npol = 10; # Degree of fittin polynomial
+my $npol = 16; # Degree of fittin polynomial
+
+# Run SRIM calculation SR.exe (1) or skip (0)
+my $sr = 1;
+#my $sr = 0;
+# Run range fit macro fit_srim_table.C (1) or skip (0)
+my $fit = 1;
+
+# $ROOTSYS directory
+my $rootsys = $ENV{ROOTSYS};
+######################################################################
+
+# Get Z, A, Element & Mass from mass table
+my @z = `awk '{print \$1}' $fmass`;
+my @a = `awk '{print \$2}' $fmass`;
+my @el = `awk '{print \$3}' $fmass`;
+my @m = `awk '{print \$4}' $fmass`;
+my $ibeg = 0;
+my $iend = 0;
+for (my $i = 0; $i < 4000; $i++){ # get the range of calbularion 
+    $z[$i] = $z[$i] + 0;
+    $a[$i] = $a[$i] + 0;
+    chomp($el[$i]);
+    $m[$i] = $m[$i] + 0.0;
+    #print "$i $z[$i] $a[$i] $el[$i] $m[$i]\n";
+    if($a[$i]==$abeg && $el[$i] eq "$elbeg"){
+	$ibeg = $i;
+    }
+    if($a[$i]==$aend && $el[$i] eq "$elend"){
+	$iend = $i;	
+	last;
+    }
+}
+# print "\n$ibeg $iend \n";
+
+my $nmat = @mat; # length of array
+for (my $j = 0; $j < $nmat; $j++){ # loop for material
+    # file definitions
+    my $mat_dir = "$dir/$mat[$j]";
+    my $mat_dat = "$mat_dir/$mat[$j].IN";
+    if(!-d $mat_dir){ # Check if directory exists
+	print "Material directory does not exist. \n";
+	exit;
+    }
+    
+    # Get the material information in SR.IN format
+    open my $fh, '<', $mat_dat or die "Error: $!";
+    my @lmat = <$fh>;
+    close $fh;
+    chomp(@lmat);
+	
+    for (my $i = $ibeg; $i < $iend+1; $i++){ # loop for isotope	    
+	if ($z[$i] < 93) { # to skip elements above U
+	    # file definitions
+	    my $sr_out = "$mat_dir/$a[$i]$el[$i]_in_$mat[$j].txt"; # SR.exe output file
+	    my $fit_out = "$mat_dir/range_fit_pol${npol}_$mat[$j]$label.txt"; # Fit output file
+	    #my $fit_out = "$mat_dir/tmp.txt"; # Fit output file
+
+	    if($sr == 1){
+		# Rewrite SR.IN for each incident ion and material
+		my $Emax = $a[$i]*400000;
+		my $l_srin = << "EOF";
+---Stopping/Range Input Data (Number-format: Period = Decimal Point)
+---Output File Name
+"$sr_out"
+---Ion(Z), Ion Mass(u)
+$z[$i]   $m[$i]
+@lmat
+---Output Stopping Units (1-8)
+ 5
+---Ion Energy : E-Min(keV), E-Max(keV)
+ 1    $Emax
+EOF
+		open(SRIN, ">", "SR.IN") or die("Cannot open SR.IN: $!");
+		print SRIN "$l_srin";
+		close(SRIN);
+		system("nkf -Lw --overwrite SR.IN"); # To covert LF to CRLF!
+	    
+		# execute SRModule.exe (you need to be in SR\ Module/ directory)
+		system("wine SRModule.exe");
+	    }
+		
+	    # Extract only the data part from the range file
+	    if($fit == 1){
+		my $sr_out_cut = "tmp.txt"; 
+		open(my $in_fh, '<', $sr_out) or die "Cannot open input file: $!"; # file before
+		open(my $out_fh, '>', $sr_out_cut) or die "Cannot open output file: $!"; #file after
+		while (my $line = <$in_fh>) { # Skip lines until
+		    last if $line =~ /-----------  ---------- ---------- ----------  ----------  ----------/;
+		}
+		while (my $line = <$in_fh>) { # Stop lines at 
+		    last if $line =~ /-----------------------------------------------------------/;
+		    print $out_fh $line;
+		}
+		close($in_fh);
+		close($out_fh);    
+		
+		# Fit range and get parameters to be overwritten in $fit_par_file 
+		system("$rootsys/bin/root -b \'fit_srim_table.C($z[$i],$a[$i],\"$mat[$j]\",\"$sr_out_cut\",$npol,\"$fit_out\",\"w\")\'");
+		system("rm $sr_out_cut");
+	    }
+	}
+    } # for i
+} # for j
